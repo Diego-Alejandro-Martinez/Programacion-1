79a20e0e0cecaa82d4ee25bba6fecafc7d3971bb
Diego <diegoalejandrom3@gmail.com>
2026-01-28 16:59:40 -0600

    Mi primer Programa en C++

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "Diego ALejandro Martinez Arevalo" << endl;
+    cout << "carnet: 9959-25-1009" << endl;
+    return 0;
+}
